d3640bcd047c899a6c21618c38e0c5cbc973a2fe
Marius Popovici <marius@popovici.net>
2018-12-23 07:34:47 -0500

    GIT ignore setup

---
+++ config/sample.dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  OW_API_KEY:'"your open weather API key here"'
+})
