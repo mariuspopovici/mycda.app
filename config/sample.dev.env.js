449f519f554584219bd86901f77edc2407470c0b
Popovici <marius.popovici@abbott.com>
2019-01-14 13:58:02 -0500

    Updated sample config files with firebase config.

--- config/sample.dev.env.js
+++ config/sample.dev.env.js
@@ -3,6 +3,12 @@ const merge = require("webpack-merge");
 const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: "'development'",
-  OW_API_KEY: "'your open weather API key here between single quotes'"
+  NODE_ENV: `"development"`,
+  OW_API_KEY: `"your open weather API key here"`,
+  FB_API_KEY: `"Firebase API key here"`,
+  FB_AUTH_DOMAIN: `"Firebase auth domain"`,
+  FB_DATABASE_URL: `"Firebase database url"`,
+  FB_PROJECT_ID: `"Firebase app name"`,
+  FB_STORAGE_BUCKET: `"Firebase storage bucket"`,
+  FB_MSG_SENDER_ID: `"Firebase sender id"`
 });
--- config/sample.prod.env.js
+++ config/sample.prod.env.js
@@ -1,5 +1,11 @@
 "use strict";
 module.exports = {
-  NODE_ENV: "'production'",
-  OW_API_KEY: "'your Open Weather API key here between single quotes'"
+  NODE_ENV: `"production"`,
+  OW_API_KEY: `"your OpenWeather API key here between quotes"`,
+  FB_API_KEY: `"Firebase API key here"`,
+  FB_AUTH_DOMAIN: `"Firebase auth domain"`,
+  FB_DATABASE_URL: `"Firebase database url"`,
+  FB_PROJECT_ID: `"Firebase app name"`,
+  FB_STORAGE_BUCKET: `"Firebase storage bucket"`,
+  FB_MSG_SENDER_ID: `"Firebase sender id"`
 };
