05ed1c2d6ab627e69d9481587d8886b069e142c0
Popovici <marius.popovici@abbott.com>
2019-01-16 10:18:37 -0500

    Added upload to firebase storage

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -3,8 +3,14 @@
     <div id="upload">
       <h2>Experiments</h2>
       <h3>Start an experiment by uploading a new .FIT activity file.</h3>
+      <b-alert
+        variant="danger"
+        dismissible
+        :show="uploadError"
+        @dismissed="uploadError=false"
+      >{{uploadMessage}}</b-alert>
       <br>
-      <vue-dropzone id="uploadDropZone" :options="dropOptions"></vue-dropzone>
+      <vue-dropzone ref="uploadDropZone" id="uploadDropZone" :options="dropOptions" v-on:vdropzone-file-added="fileAdded" ></vue-dropzone>
       <br>
       <b-table
         :dark="isDark"
@@ -61,10 +67,14 @@ export default {
   data () {
     return {
       dropOptions: {
-        url: 'https://httpbin.org/post',
+        url: '/',
+        method: 'put',
+        autoQueue: false,
+        autoProcessQueue: false,
         maxFileSize: 4, // MB
-        maxFiles: 10,
+        maxFiles: 1,
         chunking: true,
+        forceChuncking: true,
         chunkSize: 500, // bytes
         dictDefaultMessage: 'Drop .FIT files here or click to select file.'
       },
@@ -77,19 +87,98 @@ export default {
         'actions'
       ],
       items: uploadedItems,
-      isDark: this.theme === 'dark'
+      isDark: this.theme === 'dark',
+      uploadError: false,
+      uploadMessage: ''
     }
   },
   components: {
     vueDropzone
   },
   props: ['theme'],
-  methods: {},
+  methods: {
+    fileAdded: function (file) {
+      let dz = this.$refs.uploadDropZone.dropzone
+      this.uploadError = false
+
+      if (dz.files.length <= dz.options.maxFiles) {
+        let reader = new FileReader()
+        reader.onload = function (event) {
+          uploadToStorage(file, event.target.result, dz,
+            // on success
+            function (downloadURL) {
+              console.log('uploaded to: ' + downloadURL)
+            },
+            // on error
+            function (error) {
+              this.uploadError = true
+              this.uploadMessage = error.message
+            }
+          )
+        }
+
+        reader.readAsDataURL(file)
+      }
+    }
+  },
   watch: {
     theme: function (value) {
       this.isDark = value === 'dark'
     }
   }
+}
+
+function uploadToStorage (file, data, dz, callback, onErrorCallback) {
+  // this is the dropzone's file preview progressbar, we're going to use this to let the user know how firebase upload is doing
+  let dzProgressBar = file.previewElement.children[2]
+
+  // create a unique id for the file to be uploaded
+  var uuidString = uuid()
+  let path = 'userdata/' + firebase.auth().currentUser.uid + '/activities/_' + uuidString + '_' + file.name
+
+  // get a ref to firebase storage root
+  let storageRef = firebase.storage().ref()
+
+  // create a reference for the file we are going to upload
+  let fileRef = storageRef.child(path)
+
+  // firebase gives you an UploadTask (promise) - we'll use this to monitor upload progress
+  let task = fileRef.putString(data, 'data_url')
+
+  // making sure progress bar is visible
+  dzProgressBar.opacity = 1
+
+  task.on(firebase.storage.TaskEvent.STATE_CHANGED,
+    function progress (snapshot) {
+      let progressString = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      dzProgressBar.children[0].style.width = progressString + '%'
+    },
+
+    // handle upload errors
+    function (error) {
+      onErrorCallback(error)
+    },
+
+    // handle success
+    function () {
+      // get a download URL and return it to the caller
+      task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        callback(downloadURL)
+      })
+
+      console.log(dzProgressBar)
+      // hiding the progress bar
+      dzProgressBar.opacity = 0
+    }
+  )
+}
+
+function uuid () {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    // eslint-disable-next-line one-var
+    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
 }
 </script>
 
