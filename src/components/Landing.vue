b747164836a46cb983e4870e427254d9f6d4abc8
Popovici <marius.popovici@abbott.com>
2019-02-06 13:04:00 -0500

    Landing page background image

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,9 +1,10 @@
 <template>
   <section :class="this.theme === 'dark' ? 'text-white' : ''">
   <b-container fluid class="text-center">
+    <div id="fullscreen_bg" class="fullscreen_bg"/>
     <b-row>
       <b-col>
-        <b-jumbotron bg-variant="dark" ref="jumbotron" id="jumbotron"  header="What's your CdA?" lead="Your aerodynamics field testing companion." >
+        <b-jumbotron :bg-variant="theme" ref="jumbotron" id="jumbotron"  header="What's your CdA?" lead="Your aerodynamics field testing companion." >
           <hr class="my-4">
           <p>
             Sign in or sign up for free now!
@@ -13,6 +14,7 @@
         </b-jumbotron>
       </b-col>
     </b-row>
+    <section :class="this.theme === 'dark' ? 'text-white' : ''">
     <b-row>
       <b-col md="4" xs="6">
         <font-awesome-icon icon="cloud-upload-alt" size="6x"/>
@@ -30,6 +32,7 @@
         <p>Compare CdA / crr data across different segments belonging to the same activity.</p>
       </b-col>
     </b-row>
+    </section>
   </b-container>
   </section>
 </template>
@@ -50,4 +53,15 @@ export default {
 </script>
 
 <style>
+.fullscreen_bg {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-size: cover;
+  background-position: 50% 50%;
+  background-image: url('/static/images/home/tuck-1200px.jpg');
+  background-repeat:repeat;
+}
 </style>
